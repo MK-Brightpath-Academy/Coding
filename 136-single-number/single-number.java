2c2d259d347bf28c1ed64dafef9d5c13346d7a72
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-10-17 16:16:19 +0530

    Time: 9 ms (23.51%) | Memory: 44.7 MB (98.57%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+       Set<Integer> set = new HashSet<>();
+    int sum = 0;
     for (int num : nums) {
-        map.put(num, map.getOrDefault(num, 0) + 1);
+        if (set.contains(num)) sum -= num;
+        else {
+            sum += num;
+            set.add(num);
+        }
     }
-    for (int num : map.keySet()) {
-        if (map.get(num) == 1) return num;
-    }
-    return -1;
+    return sum;
     }
 }
