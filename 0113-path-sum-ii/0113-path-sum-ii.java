b1505080eef8561c2234fe35d191a7a4a0947dda
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2026-08-06 15:24:58 +0530

    Time: 1 ms (99.97%), Space: 45.4 MB (74.69%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        dfs(root, targetSum, path, ans);
+
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int target,
+                     List<Integer> path,
+                     List<List<Integer>> ans) {
+
+        if (node == null)
+            return;
+
+        path.add(node.val);
+
+        // Leaf node
+        if (node.left == null && node.right == null) {
+
+            if (target == node.val) {
+                ans.add(new ArrayList<>(path));
+            }
+
+        } else {
+
+            dfs(node.left, target - node.val, path, ans);
+            dfs(node.right, target - node.val, path, ans);
+        }
+
+        // Backtracking
+        path.remove(path.size() - 1);
+    }
+}
